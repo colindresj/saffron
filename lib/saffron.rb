b8a0aa97cb7cdd0fb8e166b1d23ab1fc7842625e
JC <jorge.e.colindres@gmail.com>
2014-03-28 18:26:03 -0400

    fix rails definition conditional

--- lib/saffron.rb
+++ lib/saffron.rb
@@ -2,7 +2,7 @@ require "saffron/installer"
 require "sass" unless defined? Sass
 
 module Saffron
-  if defined? Rails && defined? Rails::Engine
+  if defined?(Rails) && defined?(Rails::Engine)
     class Engine < ::Rails::Engine
       require "saffron/engine"
     end
