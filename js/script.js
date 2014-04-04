c151b85542b11fdf7f0969889fe241fe1cbf339e
DeProdigy <alex.hint.ah@gmail.com>
2014-04-03 21:52:53 -0400

    so much

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
 document.addEventListener('DOMContentLoaded', function(){
-  var figure = document.querySelector('figure');
-  var classNumber = 1;
-  figure.addEventListener('click', function(){
+  var figure = document.querySelector('figure'),
+      classNumber = 1,
+      previousClassNumber;
+
+  figure.addEventListener('click', changeClass);
+
+  function changeClass(){
     figure.classList.remove('c-' + classNumber);
+    previousClassNumber = classNumber;
+
     classNumber = getRandomNumber();
-    console.log(classNumber);
+
+    // make sure class number is not the same as the previous one
+    while(previousClassNumber === classNumber) {
+      classNumber = getRandomNumber();
+    }
+
     figure.classList.add('c-' + classNumber);
-  });
+  }
+
 });
 
 function getRandomNumber() {
-  var max = 9;
-  var min = 1;
-  var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-  return randomNumber;
+  var max = 9, min = 1;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
